c272f5beea0329ae24b36bc223c68e925686fba6
Giovanni Toraldo <me@gionn.net>
2015-06-02 17:26:27 +0200

    Added gpg support for travis

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash -ex
-mvn deploy -Dmaven.test.skip=true --settings .travis/settings.xml
+mvn deploy -Dmaven.test.skip=true -Dgpg.skip=false -Dgpg.passphrase=$CI_DEPLOY_GPG_SECRET -Dgpg.secretKeyring=./.travis/private.gpg --settings .travis/settings.xml
