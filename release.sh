6279a827950db669703a9778b0654ec656ff29d4
Giovanni Toraldo <g.toraldo@liberologico.com>
2015-06-03 15:03:29 +0200

    updated release.sh script

--- release.sh
+++ release.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -exu
+echo Releasing $VERSION and updating $NEXT_VERSION
 mvn versions:set -DnewVersion=${VERSION} scm:checkin -Dmessage="Release $VERSION" -DgenerateBackupPoms=false
-git push --tags
+mvn scm:tag
 mvn versions:set -DnewVersion=${NEXT_VERSION}-SNAPSHOT scm:checkin -Dmessage="Preparing for next iteration" -DgenerateBackupPoms=false
-
+echo Finished!
