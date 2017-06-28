6b6b3fde6a3f48168bbc5387c9b52180a12fb961
Giovanni Toraldo <giovanni.toraldo@cloudesire.com>
2017-06-28 13:59:58 +0200

    Switch from travis to circleci
    Use maven wrapper

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash -ex
-mvn deploy -Dmaven.test.skip=true -Dgpg.skip=false -Dgpg.publicKeyring=./.travis/public.gpg -Dgpg.secretKeyring=./.travis/private.gpg --settings .travis/settings.xml
+mvn deploy -Dmaven.test.skip=true
